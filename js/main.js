80f196ed0ce81687823bb754e7492a4f9b0f544c
blarey <112769294+blarey@users.noreply.github.com>
2022-09-05 16:39:45 +0200

    chain of calls changed

--- js/main.js
+++ js/main.js
@@ -1,61 +1,31 @@
-/*
- *  Copyright (c) 2015 The WebRTC project authors. All Rights Reserved.
- *
- *  Use of this source code is governed by a BSD-style license
- *  that can be found in the LICENSE file in the root of the source
- *  tree.
- */
-
 'use strict';
 
 const videoElement = document.querySelector('video');
-//const snapshotButton = document.querySelector('button#snapshot');
-//const filterSelect = document.querySelector('select#filter');
 const videoSelect = document.querySelector('select#videoSource');
+videoSelect.onchange = getStream;
+videoElement.className =  "invert";
 
-// Put variables in global scope to make them available to the browser console.
-const video = window.video = document.querySelector('video');
-// set video with invert filter
-video.className =  "invert";
+getStream().then(getDevices).then(gotDevices);
 
-// init stream options
-const constraints = {
-  audio: false,
-  video: { facingMode: 'environment' }
-};
+function getDevices() {
+  // AFAICT in Safari this only gets default devices until gUM is called :/
+  return navigator.mediaDevices.enumerateDevices();
+}
 
-function listVideoDevice(deviceInfos) {
-  while (videoSelect.firstChild) {
-    videoSelect.removeChild(select.firstChild);
-  }
-  for (let i = 0; i !== deviceInfos.length; ++i) {
-    const deviceInfo = deviceInfos[i];
+function gotDevices(deviceInfos) {
+  window.deviceInfos = deviceInfos; // make available to console
+  console.log('Available input and output devices:', deviceInfos);
+  for (const deviceInfo of deviceInfos) {
     const option = document.createElement('option');
     option.value = deviceInfo.deviceId;
     if (deviceInfo.kind === 'videoinput') {
-      option.text = deviceInfo.label || `camera ${videoSelect.length + 1}`;
+      option.text = deviceInfo.label || `Camera ${videoSelect.length + 1}`;
       videoSelect.appendChild(option);
-    } else {
-      console.log('Some other kind of source/device: ', deviceInfo);
     }
   }
 }
 
-// iniciar video
-function gotStream(stream) {
-  window.stream = stream; // make stream available to console
-  videoElement.srcObject = stream;
-  // Refresh button list in case labels have become available
-  return navigator.mediaDevices.enumerateDevices();
-}
-
-
-// error handler
-function handleError(error) {
-  console.log('navigator.MediaDevices.getUserMedia error: ', error.message, error.name);
-}
-
-function start() {
+function getStream() {
   if (window.stream) {
     window.stream.getTracks().forEach(track => {
       track.stop();
@@ -63,14 +33,20 @@ function start() {
   }
   const videoSource = videoSelect.value;
   const constraints = {
+    audio: false,
     video: {deviceId: videoSource ? {exact: videoSource} : undefined}
   };
-  navigator.mediaDevices.getUserMedia(constraints).then(gotStream).then(listVideoDevice).catch(handleError);
+  return navigator.mediaDevices.getUserMedia(constraints).
+    then(gotStream).catch(handleError);
 }
 
-// navigator function calls
-navigator.mediaDevices.enumerateDevices().then(listVideoDevice).catch(handleError);
+function gotStream(stream) {
+  window.stream = stream; // make stream available to console
+  videoSelect.selectedIndex = [...videoSelect.options].
+    findIndex(option => option.text === stream.getVideoTracks()[0].label);
+  videoElement.srcObject = stream;
+}
 
-videoSelect.onchange = start;
-
-start();
+function handleError(error) {
+  console.error('Error: ', error);
+}
