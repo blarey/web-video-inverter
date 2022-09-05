af7e03c96daca88aa9a8a715a86f4189c500c988
blarey <112769294+blarey@users.noreply.github.com>
2022-09-05 16:49:05 +0200

    change using the same method as main.js

--- js/diagnosis.js
+++ js/diagnosis.js
@@ -3,8 +3,15 @@
 const devices = document.querySelector('ul#devices');
 
 
+getStream().then(getDevices).then(gotDevices);
+
+function getDevices() {
+  // AFAICT in Safari this only gets default devices until gUM is called :/
+  return navigator.mediaDevices.enumerateDevices();
+}
+
 // list devices
-function listVideoDevice(deviceInfos) {
+function gotDevices(deviceInfos) {
 	// clean the list
 	devices.innerHTML = '';
 	for (let i = 0; i !== deviceInfos.length; ++i) {
@@ -22,6 +29,20 @@ function listVideoDevice(deviceInfos) {
 }
 
 
+function getStream() {
+  if (window.stream) {
+    window.stream.getTracks().forEach(track => {
+      track.stop();
+    });
+  }
+  const constraints = {
+    audio: false,
+    video: true
+  };
+  return navigator.mediaDevices.getUserMedia(constraints).catch(handleError);
+}
+
+
 // error handler
 function handleError(error) {
 	const message = String('navigator.MediaDevices.getUserMedia error: ' + error.message + " " + error.name);
@@ -30,8 +51,4 @@ function handleError(error) {
 	const item = document.createElement('li');
 	item.appendChild(document.createTextNode(message));
 	devices.appendChild(item);
-	
 }
-
-navigator.mediaDevices.enumerateDevices().then(listVideoDevice).catch(handleError);
-
